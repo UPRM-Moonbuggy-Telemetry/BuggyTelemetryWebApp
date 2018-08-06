d41b451411403dd4333818a2e9c80efb0e0fd735
JavierO <javier.ortiz13@upr.edu>
2018-08-06 14:57:38 -0400

    added other components and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { GraphsComponent } from './components/graphs/graphs.component';
+import { GpsComponent } from './components/dashboard/gps/gps.component';
+import { VideoComponent } from './components/dashboard/video/video.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent,
+    GraphsComponent,
+    GpsComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule
