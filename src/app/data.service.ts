795232f1493afda0b5b012164ed1806e37868058
JavierO <javier.ortiz13@upr.edu>
2018-09-05 18:57:01 -0400

    added important TODO

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,6 +10,7 @@ export class DataService {
 
   constructor(private _http: HttpClient) { }
 
+  //TODO change this function and replace with mock JSON data
   dailyForecast() {
     return this._http.get("http://samples.openweathermap.org/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22")
       .pipe(map(result => result));
