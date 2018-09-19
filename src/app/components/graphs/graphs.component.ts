fe8b62b54c880690877ddd2c6d0024c08f0793ba
JavierO <javier.ortiz13@upr.edu>
2018-09-18 21:28:35 -0400

    we have working chart

--- src/app/components/graphs/graphs.component.ts
+++ src/app/components/graphs/graphs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { DataService } from '../../data.service';
 import { Chart } from 'chart.js';
 
@@ -8,58 +8,62 @@ import { Chart } from 'chart.js';
   styleUrls: ['./graphs.component.css']
 })
 export class GraphsComponent implements OnInit {
+  @ViewChild('canvas') canvas: ElementRef;
+
   chart = []; // This will hold our chart info
 
-    constructor(private _weather: DataService) {}
+  constructor(private _weather: DataService) {}
 
-    ngOnInit() {
-      //we an use this example to see to request data
-      // this._weather.dailyForecast()
-      //   .subscribe(res => {
-      //     let temp_max = res['list'].map(res => res.main.temp_max);
-      //     let temp_min = res['list'].map(res => res.main.temp_min);
-      //     let alldates = res['list'].map(res => res.dt)
-      //
-      //     let weatherDates = []
-      //     alldates.forEach((res) => {
-      //       let jsdate = new Date(res * 1000)
-      //       weatherDates.push(jsdate.toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric' }))
-      //     })
-      //
-      //     this.chart = new Chart('canvas', {
-      //     type: 'line',
-      //     data: {
-      //       labels: weatherDates,
-      //       datasets: [
-      //         {
-      //           data: temp_max,
-      //           borderColor: "#3cba9f",
-      //           fill: false
-      //         },
-      //         {
-      //           data: temp_min,
-      //           borderColor: "#ffcc00",
-      //           fill: false
-      //         },
-      //       ]
-      //     },
-      //     options: {
-      //       legend: {
-      //         display: false
-      //       },
-      //       scales: {
-      //         xAxes: [{
-      //           display: true
-      //         }],
-      //         yAxes: [{
-      //           display: true
-      //         }],
-      //       }
-      //     }
-      //   });
-      //   })
+  ngOnInit() {
+    //we an use this example to see to request data
+    // this._weather.dailyForecast()
+    //   .subscribe(res => {
+    //     let temp_max = res['list'].map(res => res.main.temp_max);
+    //     let temp_min = res['list'].map(res => res.main.temp_min);
+    //     let alldates = res['list'].map(res => res.dt)
+    //
+    //     let weatherDates = []
+    //     alldates.forEach((res) => {
+    //       let jsdate = new Date(res * 1000)
+    //       weatherDates.push(jsdate.toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric' }))
+    //     })
+    //
+    //     this.chart = new Chart('canvas', {
+    //     type: 'line',
+    //     data: {
+    //       labels: weatherDates,
+    //       datasets: [
+    //         {
+    //           data: temp_max,
+    //           borderColor: "#3cba9f",
+    //           fill: false
+    //         },
+    //         {
+    //           data: temp_min,
+    //           borderColor: "#ffcc00",
+    //           fill: false
+    //         },
+    //       ]
+    //     },
+    //     options: {
+    //       legend: {
+    //         display: false
+    //       },
+    //       scales: {
+    //         xAxes: [{
+    //           display: true
+    //         }],
+    //         yAxes: [{
+    //           display: true
+    //         }],
+    //       }
+    //     }
+    //   });
+    //   })
+  }
 
-      this.chart = new Chart('canvas', {
+  ngAfterViewInit(){
+    this.chart = new Chart(this.canvas.nativeElement.getContext('2d'), {
       type: 'line',
       data: {
         labels:[345,345,3456,3456,3456,345,2345,234],
@@ -90,5 +94,5 @@ export class GraphsComponent implements OnInit {
         }
       }
     });
-    }
+  }
 }
