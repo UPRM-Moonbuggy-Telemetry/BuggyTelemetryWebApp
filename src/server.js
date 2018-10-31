4e235bcd0bd0142c56850620480b8df7db2ad59c
JavierO <javier.ortiz13@upr.edu>
2018-10-31 17:59:28 -0400

    finished building server file

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const bodyParser = require('body-parser');
+const routes = require('./api/routes/dataRoutes');//importing routes
 
 const app = express();//init express
 
@@ -13,33 +14,9 @@ app.use(express.static(__dirname + '/dist/buggy-gui-angularPort'));
 //in the docs for body parser middleware
 app.use(bodyParser.json());
 
-//methods for REST
-//get all
-app.route('/api/data').get((req, res) => {
-  res.send({
-      data: [{ name: 'lilly' }, { name: 'lucy' }]//for this example we are simply creating new data
-    });
-});
-//get name
-app.route('/api/data/:name').get((req, res) => {
-  const requestedName = req.params['name'];
-  //for the purpose of this test we only return the name in a new object
-  res.send({ name: requestedName });
-});
-//post
-app.route('/api/data').post((req, res) => {
-  res.send(201, req.body);
-});
-//for the following we require reading the params (name)
-//put (update)
-app.route('/api/data/:name').put((req, res) => {
-  res.send(200, req.body);
-});
-//delete
-app.route('/api/data/:name').delete((req, res) => {
-  res.sendStatus(204);
-});
+//TODO add database connections
 
+routes(app);//reguster the route
 //send angular file to path (has to be built)
 app.get('/*', (req,res) => res.sendFile(path.join(__dirname)));
 
