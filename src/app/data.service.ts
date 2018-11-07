02c42a749f26a3136fc696b4115b6d636f85e6f0
JavierO <javier.ortiz13@upr.edu>
2018-11-07 17:55:55 -0400

    removed unused import

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
 
 export interface Cat {
   name: string;
