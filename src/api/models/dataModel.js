572dd5c2d3f230dd079fc29263c1157e31201cf5
JavierO <javier.ortiz13@upr.edu>
2018-11-14 17:38:18 -0400

    added mysql library

--- src/api/models/dataModel.js
+++ src/api/models/dataModel.js
@@ -1,1 +1,19 @@
 // TODO models for the database
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "CREATE TABLE sensorData (id INT AUTO_INCREMENT_KEY, strain INT, vibracion INT)";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
