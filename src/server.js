fe9fb77ef6c1f1f77120a3ef206f2e8932003c19
JavierO <javier.ortiz13@upr.edu>
2018-11-18 23:22:04 -0400

    added documentation commenting

--- src/server.js
+++ src/server.js
@@ -3,21 +3,23 @@ const http = require('http');
 const path = require('path');
 const bodyParser = require('body-parser');
 const dataRoutes = require('./api/routes/dataRoutes');//importing routes
-const db = require('./api/models/dataModel.js');//db modee
+const dataDB = require('./api/models/dataModel.js');//db model
 
 const app = express();//init express
 
 //assign a port
 const port = process.env.PORT || 3000;
 
-//setup of angular files (similar to setting up a static file)
+//setup of angular files dir (similar to setting up a static file)
 app.use(express.static(__dirname + '../../dist/buggy-gui-angularPort'));
+
 //in the docs for body parser middleware
 app.use(bodyParser.json());
 
-//TODO add database connections
+//register the routes
+dataRoutes(app);
 
-dataRoutes(app);//register the route
+//////////////////////////////////////////
 //send angular file to path (has to be built first with ng build)
 app.get('/*', (req,res) => res.sendFile(path.join(__dirname)));
 
