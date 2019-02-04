97f45873d20dff32aa169ab27d22c3c48fe15409
JavierO <javier.ortiz13@upr.edu>
2019-02-04 17:43:48 -0400

    fixed map instantiation bug

--- src/app/components/dashboard/gps/gps.component.ts
+++ src/app/components/dashboard/gps/gps.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { Map, getRndNumber } from '@jaortiz117/icon-map';
 
 @Component({
@@ -11,20 +11,33 @@ export class GpsComponent implements OnInit {
   @ViewChild('parent') parent: ElementRef;
   @ViewChild('element') element: ElementRef;
   @ViewChild('container') container: ElementRef;
-  constructor() { }
+
+  map = [];
+  constructor() {
+  }
 
   ngOnInit() {
-    var map = new Map(this.parent.nativeElement, this.element.nativeElement, {
-      top: 100,
-      bottom: 0
+    this.map = new Map(this.parent.nativeElement, this.element.nativeElement, {
+      top: -86.655645,
+      bottom: -86.652185
     }, {
-      top: 100,
-      bottom: 0
+      top: 34.709558,
+      bottom: 34.711180
     });
-    map.refresh({
-      y: getRndNumber(0, 100),
-      x: getRndNumber(0, 100)
+    // map.refresh({
+    //   y: getRndNumber(-86.655645, -86.652185),
+    //   x: getRndNumber(34.709558,34.711180)
+    // });
+  }
+
+  ngAfterViewInit(){
+    this.map.refresh({
+      y: -86.653,
+      x: 34.71
     });
+
+    console.log("y: " + this.element.nativeElement.style.top, ", x: " + this.element.nativeElement.style.left);
+    console.log("Paren top: " + this.parent.nativeElement.style.position + ", Paren height: " + (this.parent.nativeElement.style.top + this.parent.nativeElement.style.height));
   }
 
 }
