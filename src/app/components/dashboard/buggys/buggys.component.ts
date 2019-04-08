d64d5112fddb2a76906f1c49a14853a1c2fd0056
sxngie <songiemar.garcia@upr.edu>
2019-04-08 18:14:26 -0400

    update

--- src/app/components/dashboard/buggys/buggys.component.ts
+++ src/app/components/dashboard/buggys/buggys.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import * as h337 from 'heatmap.js';
+import { DataService } from '../../../data.service';
 
 @Component({
   selector: 'app-buggys',
@@ -7,11 +8,46 @@ import * as h337 from 'heatmap.js';
   styleUrls: ['./buggys.component.css']
 })
 export class BuggysComponent {
+  allData = [];
 
-  constructor() { }
+  constructor(private _dataService: DataService) { }
   @ViewChild('map') map: ElementRef;
 
   ngOnInit() {
+
+    setInterval(() => {
+      this._dataService.getAllData().subscribe(data => {
+        this.allData = data;
+      })
+    }, 1000);
+    setInterval(() => {this.updateHeatmap()}, 1000);
+  }
+
+  getStrain1Data(){
+    var strain_1 = []
+    for(let i=0; i<this.allData.length; i++){
+      strain_1[i] = this.allData[i].strain_sensor_1;
+    }
+    return [strain_1];
+  }
+
+  getStrain2Data(){
+    var strain_2 = []
+    for(let i=0; i<this.allData.length; i++){
+      strain_2[i] = this.allData[i].strain_sensor_2;
+    }
+    return [strain_2];
+  }
+  
+  getStrain3Data(){
+    var strain_3 = []
+    for(let i=0; i<this.allData.length; i++){
+      strain_3[i] = this.allData[i].strain_sensor_3;
+    }
+    return [strain_3];
+  }
+
+  updateHeatmap() {
     var heatmapInstance = h337.create({
       // This is where the info is stored! 
       container: this.map.nativeElement
@@ -23,18 +59,33 @@ export class BuggysComponent {
     var max = 150; // Higher the number, less saturated and extreme red splotches.
     var len = 400; // Smaller the number, more precise red area marking of the points.
     var points = [];
-    
+
     while (len--) {
+      var strain1 = this.getStrain1Data();
+      var strain2 = this.getStrain2Data();
+      var strain3 = this.getStrain3Data();
+
       var randval = Math.floor(Math.random()*100);
       max = Math.max(max, randval);
       
-      var point = {
-        x: Math.floor(Math.random()*width),
-        y: Math.floor(Math.random()*height),
-        value: randval
+      var fPoint1 = {
+        x: 362,
+        y: 220,
+        value: strain1
       };
-      points.push(point);
+      var fPoint2 = {
+        x: 362,
+        y: 130,
+        value: strain2
+      };
+      var bPoint3 = {
+        x: 245,
+        y: 174,
+        value: strain3
+      };
+      points.push(fPoint1, fPoint2, bPoint3);
     }
+    
     
     // Follow heatmap data format.
     var data = {
