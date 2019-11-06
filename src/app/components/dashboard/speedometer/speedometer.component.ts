614531465ece32740147a5de3a741d58a8185121
Maria Vilanova <maria.vilanova@upr.edu>
2019-11-06 17:19:05 -0400

    Fixed speedgauge canvas, changed ticks

--- src/app/components/dashboard/speedometer/speedometer.component.ts
+++ src/app/components/dashboard/speedometer/speedometer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-speedometer',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpeedometerComponent implements OnInit {
 
+  @ViewChild('canvas') canvas: ElementRef<HTMLCanvasElement>;
+
   speed = 0.0;
 
   constructor() { }
@@ -14,7 +16,8 @@ export class SpeedometerComponent implements OnInit {
   ngOnInit() {
 
     setInterval(() => {
-      this.speed = getRandSpeed(0.0,40.0);
+      this.speed = getRandSpeed(0.0,20.0);
+      this.canvas.nativeElement.setAttribute('data-value', this.speed.toString());
     }, 1000); 
 
     function getRandSpeed(min, max){
