678be1aa4617a62110c127e59c295b65d091be16
Everson Rodriguez <everson.rodriguez@upr.edu>
2020-01-28 01:40:15 -0400

    Added environment variables for database. Added file to git ignore

--- src/api/config/db.config.js
+++ src/api/config/db.config.js
@@ -1,7 +1,9 @@
+import {env_var} from 'src/environments/env_var'
+
 const Sequelize = require('sequelize');
-const db = new Sequelize('buggy_db', 'buggy_user', 'mo*nbu66y', {
-  host: 'localhost',
-  dialect: 'postgres',
+const db = new Sequelize(env_var.DB_NAME, env_var.DB_USER, env_var.DB_PASSWORD, {
+  host: env_var.DB_HOST,
+  dialect: env_var.DB_DIALECT,
   logging: false,
 
   pool: {
---
+++ src/environments/env_var.js
@@ -0,0 +1,7 @@
+export const env_var = {
+  DB_NAME: 'buggy_db',
+  DB_USER: 'buggy_user',
+  DB_PASSWORD: 'mo*nbu66gy',
+  DB_HOST: 'localhost',
+  DB_DIALECT: 'postgres'
+};
